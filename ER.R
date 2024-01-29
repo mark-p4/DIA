1ae5f72dec11255a074fb7b66d55285746f3b823
mark-p4 <mark.paranskij@gmail.com>
2024-01-29 20:16:14 +0100

    established baseline

--- ER.R
+++ ER.R
@@ -5,6 +5,7 @@ library(tibble)
 library(foreach)
 library(arsenal)
 library(stringdist)
+library(microbenchmark)
 
 laptop= "C:\\Users\\User\\Dropbox\\PC\\Documents\\Uni\\DIA\\files\\unpacked\\"
 pc = "C:\\Users\\Mark\\Dropbox\\PC\\Documents\\Uni\\DIA\\files\\unpacked\\"
@@ -40,10 +41,12 @@ acmFiltered = read.csv(acmPath)
 
 #make all columns containing text string into lower case and remove special characters and white spaces
 
-normText = function(x){
+normText = function(x, whiteSpace = TRUE){
   x = str_replace_all(x, "[[:punct:]]", "")
   x = str_replace_all(x, "[^[:alnum:]]", "")
-  x = str_replace_all(x, " ", "")
+  if (whiteSpace) {
+    x = str_replace_all(x, " ", "")
+  }
   x = tolower(x)
   return(x)
 }
@@ -57,17 +60,32 @@ acmFiltered$id = paste("a",1:nrow(acmFiltered), sep = "_")
 colnames(acmFiltered) = paste("a", colnames(acmFiltered), sep = "_")
 
 fullData = merge(dblpFiltered, acmFiltered)
+fullData = add_column(fullData, key = paste(fullData$d_id, fullData$a_id, sep = "-"))
+length(fullData$key)
+length(unique(fullData$key))
 
-fullData[,"yearDiff"] = abs(fullData$d_year - fullData$a_year)
+#fullData[,"yearDiff"] = abs(fullData$d_year - fullData$a_year)
+
+baseLineLV = function(fullData1){
+  fullData1[, "lvSim"] = stringsim(fullData1$d_title, fullData1$a_title, method = "lv")
+  #fullData1[, "lvPerc"] = fullData1$lvDist / max(nchar(fullData1$d_title), nchar(fullData1$a_title))
+  fullData1[, "matched"] = fullData1$lvSim >= 0.95
+  return(fullData1)
+}
+
+results = microbenchmark(baseLineLV(fullData), times = 10)
+write.csv(results,paste(originPath, "result_baseline_bench.csv", sep = ""), row.names = FALSE)
+
+fullData1 = baseLineLV(fullData)
+
+write.csv(fullData1[,c("key", "matched")], paste(originPath, "matched_Entities_Baseline.csv", sep = ""), row.names = FALSE)
   
-blockedData = fullData[fullData$yearDiff <= 2,]
 blockedData[, "lvDist"] = stringdist(blockedData$d_title, blockedData$a_title, method = "lv")
 blockedData[, "lvPerc"] = blockedData$lvDist / max(nchar(blockedData$d_title), nchar(blockedData$a_title))
+
+similar = fullData1[fullData1$matched, c("key", "d_title", "a_title", "lvSim", "matched")]
 
 hist(blockedData$lvDist)
 hist(blockedData$lvPerc)
 
 similar = blockedData[blockedData$lvPerc <= 0.05, c("d_id", "a_id", "d_title", "a_title", "lvDist", "lvPerc","d_year","a_year")]
-
-strA = "anini t ialst udy ofove rhead so feddies"
-strB = "aniti alstud yofoverhe a feddies"
